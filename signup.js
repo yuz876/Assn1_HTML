40fed0027440c4d509848903e9f80003211fdf9a
yuz876 <yuz876@ucsd.edu>
2022-05-17 19:35:37 -0700

    modify js

--- signup.js
+++ signup.js
@@ -1,11 +1,13 @@
 function logSubmit(event){
-    register.textContent  = "Registered!";
-    signin.textContent = "Signed In!"
+    if (event.submitter.className == "btn1" ){
+        registeredOrSignedIn.textContent  = "Registered!";
+    } else {
+        registeredOrSignedIn.textContent = "Signed In!";
+    }
     event.preventDefault(); // avoid refreshing
 }
 const signupform = document.getElementById('signupform');
-const register = document.getElementById('register');
-const signin = document.getElementById('signin');
-signupform.addEventListener('submit', logSubmit);
+const registeredOrSignedIn = document.querySelector("#registeredOrSignedIn")
+signupform.addEventListener("submit", logSubmit);
 
 
